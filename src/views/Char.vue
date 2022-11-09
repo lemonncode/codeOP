e01095b9e3a35e3f6fce06c0085149d3e619d501
Noelia <lemmonn.87@gmail.com>
2022-11-09 14:54:53 +0100

    first commit

---
+++ src/views/Char.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <!-- {{ char.name }}
+    {{ char.birth_year }}
+    {{ char.gender }}
+    {{ char.height }}
+    {{ char.mass }} -->
+    {{ char }}
+  </div>
+</template>
+
+<script>
+export default {
+    props:['char'],
+  data() {
+    return {
+      
+      id: 2,
+    };
+  },
+}
+
+
+
+
+
+
+
+
+
+
+//   created() {
+ 
+//     // this.id = this.$route.params.id
+//   },
+
+//   mounted() {
+//     this.getChar();
+//   },
+//   methods: {
+//     async getChar() {
+//       await fetch(`https://swapi.dev/api/people/${this.id}`)
+//         .then((response) => response.json())
+//         //.then((data) => console.log(data))
+
+//         .then((data) => (this.char = data));
+//     },
+    
+//   },
+// };
+</script>
+
+<style>
+</style>
